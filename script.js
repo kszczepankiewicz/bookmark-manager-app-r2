706622005a70f6a6d5a7bc0e1f15cb0f2bc8281e
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-10 19:10:47 +0200

    feat: add adding bookmarks

---
+++ script.js
@@ -0,0 +1,68 @@
+const categoryList = document.getElementById('category-list');
+
+const mainSection = document.getElementById('main-section');
+const formSection = document.getElementById('form-section');
+const bookmarkListSection = document.getElementById('bookmark-list-section');
+
+const nameEl = document.getElementById('name');
+const url = document.getElementById('url');
+const categoryDropdown = document.getElementById('category-dropdown');
+
+const addBookmarkButton = document.getElementById('add-bookmark-button');
+const closeFormButton = document.getElementById('close-form-button');
+const addBookmarkButtonForm = document.getElementById('add-bookmark-button-form');
+const viewCategoryButton = document.getElementById('view-category-button');
+
+const categoryNames = document.getElementsByClassName('category-name');
+
+const getBookmarks = () => {
+    const raw = localStorage.getItem('bookmarks');
+    if (!raw) return []
+
+    let bookmarks;
+    try {
+        bookmarks = JSON.parse(raw);
+    }
+    catch (error) {
+        return [];
+    }
+
+    const props = ['name', 'category', 'url'];
+    if (!bookmarks.every(obj => props.every(p => Object.hasOwn(obj, p)))) return [];
+
+    return bookmarks;
+}
+
+const displaySection = section => {
+    const sections = [mainSection, formSection];
+    sections.forEach(s => s.classList.add('hidden'));
+
+    section.classList.remove('hidden');
+}
+
+const displayOrCloseForm = () => displaySection(mainSection.classList.contains('hidden') ? mainSection : formSection);
+
+addBookmarkButton.addEventListener('click', (e) => {
+    Array.from(categoryNames).forEach(el => el.textContent = categoryDropdown.value);
+    displayOrCloseForm();
+})
+
+closeFormButton.addEventListener('click', displayOrCloseForm);
+
+addBookmarkButtonForm.addEventListener('click', (e) => {
+    const bookmarks = getBookmarks();
+    bookmarks.push({ name: nameEl.value, category: categoryDropdown.value, url: url.value });
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    nameEl.value = '';
+    url.value = '';
+    displayOrCloseForm();
+});
+
+const displayOrHideCategory = () => displaySection(mainSection.classList.contains('hidden') ? mainSection : bookmarkListSection);
+
+viewCategoryButton.addEventListener('click', (e) => {
+    debugger
+    const bookmarks = getBookmarks().filter(obj => obj.category === categoryDropdown.value);
+    // const bookmarks = getBookmarks().filter(({ category }) => category === categoryDropdown.value);
+    if (!bookmarks.length) categoryList.innerHTML = `<p>No Bookmarks Found</p>`;
+});
